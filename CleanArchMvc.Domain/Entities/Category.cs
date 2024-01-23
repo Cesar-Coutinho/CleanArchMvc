dded210ed4c99fd4a2b62dc0123de993c6aedd49
Cesar Coutinho <CesarCoutinho31@gmail.com>
2024-01-22 21:57:04 -0300

    1 - Add domain folders | 2 - Add entities class

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public ICollection<Product> Products { get; private set; }
+
+
+        public Category() 
+        {            
+        }
+    }
+}
